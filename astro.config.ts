1267154cb11a25ec3cf52d4b6b602d0b0a1c0e32
Marc Vila <marc@mhou.es>
2026-03-24 17:09:55 +0100

    change 2

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	base: '/bc/nuestrohogar/',
+	//base: '/bc/nuestrohogar/',
 	devToolbar: { enabled: false },
 	vite: { plugins: [tailwindcss()] },
 })
